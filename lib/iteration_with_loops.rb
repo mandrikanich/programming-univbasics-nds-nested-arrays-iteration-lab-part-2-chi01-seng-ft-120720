e2211a8e05085ca664faf74f1161ca1fbcdcc8a2
Michael Andrikanich <mandrikanich@gmail.com>
2020-11-25 17:52:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@
      lowest_value = 100
      while element_index < src[row_index].count do
        binding.pry
-         if src[row_index][element_index].length > lowest_value.length
+         if src[row_index][element_index] < lowest_value
             lowest_value = src[row_index][element_index]
          end
            element_index += 1
