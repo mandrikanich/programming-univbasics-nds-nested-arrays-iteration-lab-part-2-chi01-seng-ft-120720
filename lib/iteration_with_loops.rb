cfd721b53158a54529bd1b3f4a8f5bc3926def5e
Michael Andrikanich <mandrikanich@gmail.com>
2020-11-25 17:26:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,17 +3,13 @@
    binding.pry 
    outer_results = []
    row_index = 0
-   while row_index < spice_rack.count do
+   while row_index < src.count do
      element_index = 0
      longest_string_element = ""
-     while element_index < spice_rack[row_index].count do
- 
-    # Array at row_index
-    # Element of the inner array at element_index
-    # If the length of the current element is greater than the length of longest_string_element
-    # Set longest_string_element to the current element
-         if spice_rack[row_index][element_index].length > longest_string_element.length
-            longest_string_element = spice_rack[row_index][element_index]
+     while element_index < src[row_index].count do
+       
+         if src[row_index][element_index].length > longest_string_element.length
+            longest_string_element = src[row_index][element_index]
          end
            element_index += 1
      end
